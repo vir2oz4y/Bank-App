6813b9ccbb749bc5a26a9d837f07d43f19b256b2
vir2oz4y <nicklson1@yandex.ru>
2020-10-08 21:21:06 +0700

    create Bank app

---
+++ Bank/DemandAccount.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Bank
+{
+    class DemandAccount:Account
+    {
+        public DemandAccount(decimal sum, int percentage) : base(sum, percentage)
+        {
+
+        }
+
+        protected internal override void Open()
+        {
+            base.OnOpened(new AccountEventArgs($"Открыт новый счет до востребования! Id счета: {this.Id}", this.Sum));
+        }
+    }
+}
---
+++ Bank/IAccount.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Bank
+{
+    interface IAccount
+    {
+        void Put(decimal sum);
+        decimal Withdraw(decimal sum);
+    }
+}
